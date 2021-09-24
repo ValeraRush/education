92786492981bd5f0c601c3d839540aa9ab1ce5b7
ValeraRush <rushexceedteam@gmail.com>
2021-09-24 12:59:43 +0300

    Basic 1.1

--- 1. Basic/1) basic-sum/index.js
+++ 1. Basic/1) basic-sum/index.js
@@ -1,8 +1,8 @@
-const a = 10
-const b = 5
-
-function arrSum(b, a) {
-    return a + b
+function sum(a, b) {
+    return a + b;
 }
 
-console.log(arrSum(b, a));
+window.sum = sum;
+
+export default sum;
+
