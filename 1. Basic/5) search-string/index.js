c3e82f55e7a23204ac9197485c4c10a0074a9947
ValeraRush <rushexceedteam@gmail.com>
2021-09-29 13:34:05 +0300

    search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,7 +1,17 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+    let text = '?'
+    let mass = []
+    if (!!Object.keys(obj).length) {
+        for (let elem in obj) {
+            if (obj[elem] !== null && typeof obj[elem] !== 'undefined' && obj[elem] !== '') {
+                mass.push(elem + '=' + obj[elem])
+            }
+        }
+        return text += mass.join('&')
+    }else {
+        return ''
+    }
 }
-
 window.searchString = searchString;
 
 export default searchString;
