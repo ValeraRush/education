307d1fa80d9dc7c208a574e5c088775e90afa11d
ValeraRush <rushexceedteam@gmail.com>
2021-09-30 14:43:49 +0300

    sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,7 +1,18 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+    try {
+        if ((Number(a) && Number(b)) &&
+            (isNaN(a) && isNaN(b)) &&
+            (a === 0 && b === 0) ||
+            (typeof a !== 'string' && typeof b !== 'string') &&
+            (typeof a !== 'object' && typeof b !== 'object') &&
+            (typeof a !== 'boolean'&& typeof b !== 'boolean')){
+            return a + b
+        }
+
+    } catch (error) {
+
+    }
+    throw new Error('Wrong arguments type!')
 }
-
 window.sumCheck = sumCheck;
-
 export default sumCheck;
